37cac2c1a77b921d479b98eb960ea45f55723b0e
Dean Wette <wetted@objectcomputing.com>
2023-02-09 12:53:06 -0600

    Cherry-pick from Fixing issue where tests didn't run under docs-examples, and some build cleanup/reorg.
    fixes #431

--- docs-examples/example-groovy/src/test/groovy/io/micronaut/rabbitmq/docs/consumer/concurrent/ConcurrentSpec.groovy
+++ docs-examples/example-groovy/src/test/groovy/io/micronaut/rabbitmq/docs/consumer/concurrent/ConcurrentSpec.groovy
@@ -7,11 +7,13 @@ class ConcurrentSpec extends AbstractRabbitMQTest {
     void "test concurrent consumers"() {
         startContext()
 
+        when:
         ProductClient productClient = applicationContext.getBean(ProductClient)
         4.times { productClient.send("body".bytes) }
 
         ProductListener productListener = applicationContext.getBean(ProductListener)
 
+        then:
         waitFor {
             productListener.threads.size() == 4
         }
