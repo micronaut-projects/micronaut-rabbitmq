5a69b3661a2b7b60d6e9810dd3980e4a898687d6
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-01 18:28:14 +0200

    depend on json.core not serde

--- rabbitmq/build.gradle.kts
+++ rabbitmq/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
     api(mn.micronaut.inject)
     api(libs.managed.amqp.client)
 
-    implementation(mnSerde.micronaut.serde.support)
-    implementation(mnSerde.micronaut.serde.jackson)
+    implementation(mn.micronaut.json.core)
+
     implementation(mn.micronaut.retry)
     implementation(mn.reactor)
     implementation(libs.caffeine)
@@ -20,6 +20,7 @@ dependencies {
     compileOnly(mn.micronaut.management)
     compileOnly(mnMicrometer.micronaut.micrometer.core)
 
+    testImplementation(mnSerde.micronaut.serde.jackson)
     testImplementation(libs.testcontainers.spock)
     testImplementation(mn.micronaut.inject.groovy)
     testImplementation(mn.micronaut.inject.java)
